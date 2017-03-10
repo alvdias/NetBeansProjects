fb0f52a4aca09c62d275480a6cfcb58e157700d9
Diego Varela <diego.varela@sinetic.com.br>
2017-03-10 20:28:41 -0300

    Update

---
+++ TesteSoftwate/test/NumeroAleatorioTest.java
@@ -0,0 +1,102 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.util.ArrayList;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import testesoftware.NumeroAleatorio;
+
+/**
+ *
+ * @author diego
+ */
+public class NumeroAleatorioTest {
+
+    public NumeroAleatorioTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void casoDeTeste01() {
+        NumeroAleatorio numeroAleatorio = new NumeroAleatorio();
+        int resp = numeroAleatorio.gerarNumeroAleatorio(1, 10);
+        assertTrue(resp >= 1 && resp <= 10);
+    }
+
+    @Test
+    public void casoDeTeste02() {
+        NumeroAleatorio numeroAleatorio = new NumeroAleatorio();
+        int resp = numeroAleatorio.gerarNumeroAleatorio(-1, 10);
+        assertEquals(-1, resp);
+    }
+
+    @Test
+    public void casoDeTeste03() {
+        NumeroAleatorio numeroAleatorio = new NumeroAleatorio();
+        char s[] = {'a', 'b', 'c', 'd', 'e'};
+        String resp = numeroAleatorio.acharCaracter(5, s, 'c');
+        assertEquals("2", resp);
+    }
+
+    @Test
+    public void casoDeTeste04() {
+        NumeroAleatorio numeroAleatorio = new NumeroAleatorio();
+        char s[] = {'a', 'b', 'c', 'd', 'e'};
+        String resp = numeroAleatorio.acharCaracter(5, s, 'f');
+        assertEquals("caracter nao encontrado", resp);
+    }
+
+    @Test
+    public void casaDeTeste05() {
+        ArrayList<String> list = new ArrayList<>();
+        list.add("andre");
+        list.add("joao");
+        list.add("josefa");
+        list.add("maria");
+
+        try {
+            list.get(5);
+            fail("Não deveria chegar aqui!");
+        } catch (Exception e) {
+            assertTrue(e instanceof IndexOutOfBoundsException);
+        }
+    }
+
+    @Test
+    public void casaDeTeste06() {
+        ArrayList<String> list = new ArrayList<>();
+        list.add("andre");
+        list.add("joao");
+        list.add("josefa");
+        list.add("maria");
+
+        try {
+            String resp =  list.get(2);
+            assertEquals(resp, "josefa");
+        } catch (Exception e) {
+            fail("Não deveria chegar aqui!");
+        }
+    }
+}
--- pooes/src/janelas/Janela01.java
+++ pooes/src/janelas/Janela01.java
@@ -29,78 +29,203 @@ public class Janela01 extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        btnPrinOk = new javax.swing.JButton();
-        jPanel1 = new javax.swing.JPanel();
-        btnPrinCancela = new javax.swing.JButton();
+        buttonGroupBebidas = new javax.swing.ButtonGroup();
+        jButtonCancelar = new javax.swing.JButton();
+        jLabelNome = new javax.swing.JLabel();
+        jTextNome = new javax.swing.JTextField();
+        jLabelIdade = new javax.swing.JLabel();
+        jSpinnerIdade = new javax.swing.JSpinner();
+        jCheckBoxSobremesa = new javax.swing.JCheckBox();
+        jCheckBoxSalada = new javax.swing.JCheckBox();
+        jCheckBoxBebida = new javax.swing.JCheckBox();
+        jCheckBoxPratoDia = new javax.swing.JCheckBox();
+        jButtonPedir = new javax.swing.JButton();
+        jRadioButtonSuco = new javax.swing.JRadioButton();
+        jRadioButtonAgua = new javax.swing.JRadioButton();
+        jRadioButtonRefrigerante = new javax.swing.JRadioButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        btnPrinOk.setText("Ok");
-        btnPrinOk.addMouseListener(new java.awt.event.MouseAdapter() {
+        jButtonCancelar.setText("Cancelar");
+        jButtonCancelar.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
-                btnPrinOkMouseClicked(evt);
+                jButtonCancelarMouseClicked(evt);
             }
         });
 
-        btnPrinCancela.setText("Cancela");
-        btnPrinCancela.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btnPrinCancelaActionPerformed(evt);
+        jLabelNome.setText("Nome");
+
+        jLabelIdade.setText("Idade");
+
+        jSpinnerIdade.setModel(new javax.swing.SpinnerNumberModel(0, 0, null, 1));
+        jSpinnerIdade.addChangeListener(new javax.swing.event.ChangeListener() {
+            public void stateChanged(javax.swing.event.ChangeEvent evt) {
+                jSpinnerIdadeStateChanged(evt);
             }
         });
 
-        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
-        jPanel1.setLayout(jPanel1Layout);
-        jPanel1Layout.setHorizontalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addComponent(btnPrinCancela)
-                .addGap(0, 12, Short.MAX_VALUE))
-        );
-        jPanel1Layout.setVerticalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                .addGap(0, 0, Short.MAX_VALUE)
-                .addComponent(btnPrinCancela))
-        );
+        jCheckBoxSobremesa.setText("Sobremesa");
+
+        jCheckBoxSalada.setText("Salada");
+
+        jCheckBoxBebida.setText("Bebida");
+        jCheckBoxBebida.setEnabled(false);
+
+        jCheckBoxPratoDia.setText("Prato do Dia");
+
+        jButtonPedir.setText("Pedir");
+        jButtonPedir.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jButtonPedirMouseClicked(evt);
+            }
+        });
+
+        buttonGroupBebidas.add(jRadioButtonSuco);
+        jRadioButtonSuco.setText("Suco");
+
+        buttonGroupBebidas.add(jRadioButtonAgua);
+        jRadioButtonAgua.setText("Água");
+
+        buttonGroupBebidas.add(jRadioButtonRefrigerante);
+        jRadioButtonRefrigerante.setText("Refrigerante");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addContainerGap(504, Short.MAX_VALUE)
-                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(btnPrinOk)
+                .addGap(22, 22, 22)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(1, 1, 1)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jCheckBoxSobremesa)
+                            .addComponent(jCheckBoxPratoDia)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(jCheckBoxBebida)
+                                    .addComponent(jCheckBoxSalada))
+                                .addGap(91, 91, 91)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(jRadioButtonRefrigerante)
+                                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                                        .addComponent(jRadioButtonSuco)
+                                        .addComponent(jRadioButtonAgua))
+                                    .addGroup(layout.createSequentialGroup()
+                                        .addGap(0, 85, Short.MAX_VALUE)
+                                        .addComponent(jButtonCancelar)
+                                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                        .addComponent(jButtonPedir))))))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(jLabelNome)
+                            .addComponent(jLabelIdade))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jTextNome)
+                            .addComponent(jSpinnerIdade))))
                 .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(268, Short.MAX_VALUE)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(btnPrinOk)
-                    .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap())
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jTextNome, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabelNome))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jSpinnerIdade, javax.swing.GroupLayout.PREFERRED_SIZE, 28, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabelIdade))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jCheckBoxSobremesa)
+                    .addComponent(jRadioButtonSuco))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jCheckBoxSalada)
+                    .addComponent(jRadioButtonAgua, javax.swing.GroupLayout.PREFERRED_SIZE, 18, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jCheckBoxBebida)
+                    .addComponent(jRadioButtonRefrigerante))
+                .addGap(18, 18, 18)
+                .addComponent(jCheckBoxPratoDia)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 49, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jButtonPedir)
+                    .addComponent(jButtonCancelar))
+                .addGap(16, 16, 16))
         );
 
-        setSize(new java.awt.Dimension(660, 331));
+        setSize(new java.awt.Dimension(429, 345));
         setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
-    private void btnPrinCancelaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnPrinCancelaActionPerformed
+    private void jButtonCancelarMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButtonCancelarMouseClicked
         // TODO add your handling code here:
-    }//GEN-LAST:event_btnPrinCancelaActionPerformed
+        limpaTela();
+    }//GEN-LAST:event_jButtonCancelarMouseClicked
 
-    private void btnPrinOkMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnPrinOkMouseClicked
+    private void jButtonPedirMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButtonPedirMouseClicked
         // TODO add your handling code here:
+        String nome = jTextNome.getText();
+        int idade = (int) jSpinnerIdade.getValue();
+        StringBuilder pedido = new StringBuilder();
+        if (nome.equals("")) {
+            JOptionPane.showMessageDialog(
+                this,
+                "Nome em Branco!",
+                "Erro!",
+                JOptionPane.ERROR_MESSAGE);
+            return;
+        }
+        pedido.append("Pessoa: " + nome + "\n");
+        pedido.append("Idade: " + idade + "\n");
+        pedido.append("Produtos: \n");
+        boolean sobremesa = jCheckBoxSobremesa.isSelected();
+        boolean salada = jCheckBoxSalada.isSelected();
+        boolean bebida = jCheckBoxBebida.isSelected();
+        boolean pratoDia = jCheckBoxPratoDia.isSelected();
+        boolean suco = jRadioButtonSuco.isSelected();
+        boolean agua = jRadioButtonAgua.isSelected();
+        boolean refrigerante = jRadioButtonRefrigerante.isSelected();
+        if (sobremesa) {
+            pedido.append("+ Sobremesa\n");
+        }
+        if (salada) {
+            pedido.append("+ Salada\n");
+        }
+        if (bebida) {
+            pedido.append("+ Bebida\n");
+        }
+        if (pratoDia) {
+            pedido.append("+ Prato do dia\n");
+        }
+        if (suco) {
+            pedido.append("+ Suco\n");
+        } else if (agua) {
+            pedido.append("+ Agua\n");
+        } else if (refrigerante) {
+            pedido.append("+ Refrigerante");
+        }
         JOptionPane.showMessageDialog(
                 this,
-                "Você clicou no Ok!",
-                "Janela - Ok!",
-                JOptionPane.ERROR_MESSAGE);
-    }//GEN-LAST:event_btnPrinOkMouseClicked
+                pedido.toString(),
+                "Pedido",
+                JOptionPane.PLAIN_MESSAGE);
+        limpaTela();
+    }//GEN-LAST:event_jButtonPedirMouseClicked
+
+    private void jSpinnerIdadeStateChanged(javax.swing.event.ChangeEvent evt) {//GEN-FIRST:event_jSpinnerIdadeStateChanged
+        // TODO add your handling code here:
+        int idade = (int) jSpinnerIdade.getValue();
+        if (idade > 17) {
+            jCheckBoxBebida.setEnabled(true);
+        } else {
+            jCheckBoxBebida.setEnabled(false);
+        }
+    }//GEN-LAST:event_jSpinnerIdadeStateChanged
 
     /**
      * @param args the command line argumentsp
@@ -138,8 +263,31 @@ public class Janela01 extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton btnPrinCancela;
-    private javax.swing.JButton btnPrinOk;
-    private javax.swing.JPanel jPanel1;
+    private javax.swing.ButtonGroup buttonGroupBebidas;
+    private javax.swing.JButton jButtonCancelar;
+    private javax.swing.JButton jButtonPedir;
+    private javax.swing.JCheckBox jCheckBoxBebida;
+    private javax.swing.JCheckBox jCheckBoxPratoDia;
+    private javax.swing.JCheckBox jCheckBoxSalada;
+    private javax.swing.JCheckBox jCheckBoxSobremesa;
+    private javax.swing.JLabel jLabelIdade;
+    private javax.swing.JLabel jLabelNome;
+    private javax.swing.JRadioButton jRadioButtonAgua;
+    private javax.swing.JRadioButton jRadioButtonRefrigerante;
+    private javax.swing.JRadioButton jRadioButtonSuco;
+    private javax.swing.JSpinner jSpinnerIdade;
+    private javax.swing.JTextField jTextNome;
     // End of variables declaration//GEN-END:variables
+
+    private void limpaTela() {
+        jTextNome.setText("");
+        jSpinnerIdade.setValue(0);
+        jCheckBoxSobremesa.setSelected(false);
+        jCheckBoxSalada.setSelected(false);
+        jCheckBoxBebida.setSelected(false);
+        jCheckBoxPratoDia.setSelected(false);
+        jRadioButtonSuco.setSelected(false);
+        jRadioButtonAgua.setSelected(false);
+        jRadioButtonRefrigerante.setSelected(false);
+    }
 }
