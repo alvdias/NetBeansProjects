482f5e2a6f2566440a1610422b667348c75bcd45
Diego Varela <diego.varela@sinetic.com.br>
2017-03-15 21:24:32 -0300

    Update

--- TesteSoftwate/test/numero/NumeroAleatorio2Test.java
+++ TesteSoftwate/test/numero/NumeroAleatorio2Test.java
@@ -1,3 +1,5 @@
+package numero;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
--- TesteSoftwate/test/numero/NumeroAleatorioTest.java
+++ TesteSoftwate/test/numero/NumeroAleatorioTest.java
@@ -1,3 +1,5 @@
+package numero;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -61,7 +63,7 @@ public class NumeroAleatorioTest {
         assertEquals("2", resp);
     }
 
-    @Ignore
+    //@Ignore
     @Test
     public void casoDeTeste04() {
         char s[] = {'a', 'b', 'c', 'd', 'e'};
