92382c60cab1bfd742bdcdaa3f20f5facf412ff2
Diego Varela <diego.varela@sinetic.com.br>
2017-03-16 22:50:20 -0300

    Update

--- pooes/src/arquivos/TestaArquivo.java
+++ pooes/src/arquivos/TestaArquivo.java
@@ -5,35 +5,66 @@
  */
 package arquivos;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import javax.swing.JOptionPane;
 
 /**
  *
  * @author diego
  */
 public class TestaArquivo {
+
     public static void main(String[] args) throws IOException {
-        // O objeto file representa uma abstração que permite
-        // acesso a um arquivo ou diretorio.
+        //O objeto file representa uma abstracao que permite
+        //acesso a um arquivo ou diretorio.
+        //Construtor usa caminho completo, naso cria o arquivo
         File diretorio = new File("/home/diego/temp/");
         diretorio.mkdir();
         File arquivo = new File(diretorio, "lixo.txt");
+        
+        //File writer cria arquivo
+        //Sobrescreve o arquivo
+        //FileWriter escritor = new FileWriter(arquivo);
+        //Nao sobrescreve
+        FileWriter escritor = new FileWriter("/home/diego/temp/log.txt", false);
+        BufferedWriter escritorBuffer = new BufferedWriter(escritor);
+        escritorBuffer.write("Ola javoso!");
+        escritorBuffer.close();
+        //Descarrega sem fechar
+        //escritorBuffer.flush(); 
+        escritor.close();
+
+        FileReader leitor = new FileReader(arquivo);
+        BufferedReader leitorBuffer = new BufferedReader(leitor);
+        System.out.println("Leitura do arquivo: ");
+        while(leitorBuffer.ready()) {
+            JOptionPane.showMessageDialog(null, leitorBuffer.readLine());
+            System.out.println(leitorBuffer.readLine());
+        }
+        leitorBuffer.close();
+        leitor.close();
+        
         File subdir = new File(diretorio, "subdir1");
         subdir.mkdir();
         String[] arquivos = diretorio.list();
-        for (int i = 0; i < arquivos.length; i++) {
-            File filho = new File(diretorio, arquivos[i]);
+        for (String arquivo1 : arquivos) {
+            File filho = new File(diretorio, arquivo1);
             System.out.println(filho.getAbsolutePath());
             System.out.println(filho.getParent());
             System.out.println("É diretorio? " + filho.isDirectory());
             System.out.println("É arquivo? " + filho.isFile());
             System.out.println("Tamanho = " + filho.length() + " bytes.");
         }
-        if (arquivo.exists()) {
-            arquivo.delete();
-        } else {
-            arquivo.createNewFile();
-        }
+//        if (arquivo.exists()) {
+//            arquivo.delete();
+//        } else {
+//            não sobrescreve se ja existe
+//            arquivo.createNewFile();
+//        }
     }
 }
---
+++ pooes/src/arquivos/TestaJChooser.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arquivos;
+
+import java.io.File;
+import javax.swing.JFileChooser;
+
+/**
+ *
+ * @author diego
+ */
+public class TestaJChooser {
+    
+    public static void main(String[] args) {
+        File file;
+        file = getArquivo("Selecionar");
+    }
+    
+    public static File getArquivo(String txtbtn) {
+        File arquivo = null;
+        String pastainicial = System.getProperty("user.dir");
+        //caso queiram saber mais:
+        System.out.println(System.getProperties());
+        JFileChooser chooser = new JFileChooser(pastainicial);
+        //chooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+        //ou
+        //chooser.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+        //ou
+        //chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+        int opcao = chooser.showDialog(null, txtbtn);
+        if (opcao == JFileChooser.APPROVE_OPTION) {
+            //pega arquivo
+            arquivo = chooser.getSelectedFile();
+            System.out.println("O Arquivo selecionado foi: "
+                    + arquivo.getAbsolutePath());
+        } else if (opcao == JFileChooser.CANCEL_OPTION) {
+            return null;
+        }
+        return arquivo;
+    }
+}
