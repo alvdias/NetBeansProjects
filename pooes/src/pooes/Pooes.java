cbac7169d035345b51657951f889f397f464a1d1
Diego Varela <diego.varela@sinetic.com.br>
2017-03-21 19:20:21 -0300

    Update

--- pooes/src/pooes/Pooes.java
+++ pooes/src/pooes/Pooes.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package pooes;
 
 /**
@@ -11,9 +6,6 @@ package pooes;
  */
 public class Pooes {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Olá " + args[0]);
